5f94512a01f97834592fff292815dddbcd8df2e8
Palace <Palace@MOON-PALACE>
2020-09-24 23:45:29 +0530

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	constexpr int WORD_LENGTH = 5;
+	cout << "Welcome to Bulls and Cows Game, a fun word game." << endl;
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
+	return 0;
+}
